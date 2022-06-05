90497f659eb7a24ebf7dbe17ddad286163c59a52
Logedbog <68834352+Logedbog@users.noreply.github.com>
2022-06-04 22:10:28 -0400

    Update and rename GhostWareAimbot.lua to GhostWareAimbotOn.lua

--- GhostWareAimbot.lua
+++
@@ -1,7 +0,0 @@
-
-_G.AimbotInput = "RightClick" -- // RightClick, LeftClick, Q, etc...
-_G.AimbotEasing = 0.2 -- // Stage of Linear Easing to target when enabled
-_G.TeamCheck = true -- // Checks the team of the target to make sure they're not on your team
-
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Logedbog/Hub/main/SomeAimbot.lua"))()
